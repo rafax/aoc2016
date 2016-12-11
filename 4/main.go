b10eee42cfeffea3d994d4e8bcfcd3f347ca960c
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-12-11 20:47:21 +0100

    Part 1 done

--- 4/main.go
+++ 4/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -22,14 +24,21 @@ func main() {
 	// } else {
 	// 	pos = Position{x: 1, y: 3, keyboard: keyboard2}
 	// }
+	sum := 0
 	for scanner.Scan() {
-
+		res := scanner.Text()
+		r := parseRoom(res)
+		if r.isReal() {
+			sum += r.sectorID
+		}
 	}
+	fmt.Println(sum)
 }
 
 type room struct {
-	name     []rune
+	counts   map[rune]int
 	sectorID int
+	checksum string
 }
 
 type pair struct {
@@ -37,20 +46,35 @@ type pair struct {
 	v int
 }
 
-func (r room) isReal() {
-	cnt := map[rune]int{}
-	for _, v := range r.name {
-		cnt[v]++
-	}
+func (r room) isReal() bool {
 	pairs := []pair{}
-	for k, v := range cnt {
+	for k, v := range r.counts {
 		pairs = append(pairs, pair{k, v})
 	}
 	sort.Slice(pairs, func(i, j int) bool {
 		if pairs[i].v == pairs[j].v {
 			return pairs[i].k < pairs[j].k
 		}
-		return pairs[i].v < pairs[j].v
+		return pairs[i].v > pairs[j].v
 	})
-	fmt.Println(pairs)
+	f5 := []string{}
+	for _, v := range pairs[:5] {
+		f5 = append(f5, string(v.k))
+	}
+	sum := strings.Join(f5, "")
+	fmt.Println(r.checksum, sum)
+	return r.checksum == sum
+}
+
+func parseRoom(in string) room {
+	cnt := map[rune]int{}
+	parts := strings.Split(in, "-")
+	for _, p := range parts[:len(parts)-1] {
+		for _, v := range p {
+			cnt[v]++
+		}
+	}
+	end := strings.Split(parts[len(parts)-1], "[")
+	id, _ := strconv.Atoi(end[0])
+	return room{counts: cnt, sectorID: id, checksum: end[1][:len(end[1])-1]}
 }
