b3162d19dd6cebf5698bfe0c5af54c00871d56a3
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-12-19 17:25:49 +0100

    Part1 works

--- 19/main.go
+++ 19/main.go
@@ -8,12 +8,35 @@ import (
 func main() {
 	var res string
 	if os.Getenv("PART") == "1" {
-		res = solve("11111")
+		res = solve(3001330)
 	} else {
 
 	}
 	fmt.Println(res)
 }
-func solve(in string) string {
-	return ""
+func solve(cnt int) string {
+	own := make([]int, cnt)
+	for i := 0; i < cnt; i++ {
+		own[i] = 1
+	}
+	i := 0
+	for {
+		next := (i + 1) % len(own)
+		if own[i] > 0 {
+			stealFrom := next
+			for {
+				if own[stealFrom] > 0 {
+					own[i] += own[stealFrom]
+					own[stealFrom] = 0
+					break
+				}
+
+				stealFrom = (stealFrom + 1) % len(own)
+			}
+		}
+		if own[i] == cnt {
+			return fmt.Sprint(i + 1)
+		}
+		i = next
+	}
 }
