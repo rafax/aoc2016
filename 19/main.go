12759e1a164e111f7d14c31dd3b3bfa2e6c7531d
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-12-20 10:14:51 +0100

    Solved both optimized

--- 19/main.go
+++ 19/main.go
@@ -1,42 +1,89 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"os"
 )
 
 func main() {
+	// f, err := os.Create("p2.prof")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+
 	var res string
 	if os.Getenv("PART") == "1" {
-		res = solve(3001330)
+		res = part1(3001330)
 	} else {
-
+		res = part2(3001330)
 	}
 	fmt.Println(res)
 }
-func solve(cnt int) string {
-	own := make([]int, cnt)
-	for i := 0; i < cnt; i++ {
-		own[i] = 1
-	}
-	i := 0
-	for {
-		next := (i + 1) % len(own)
-		if own[i] > 0 {
-			stealFrom := next
-			for {
-				if own[stealFrom] > 0 {
-					own[i] += own[stealFrom]
-					own[stealFrom] = 0
-					break
-				}
 
-				stealFrom = (stealFrom + 1) % len(own)
-			}
-		}
-		if own[i] == cnt {
-			return fmt.Sprint(i + 1)
-		}
-		i = next
+type elf struct {
+	index, value int
+}
+
+func (e *elf) SetValue(v int) {
+	e.value = v
+}
+
+func part1(cnt int) string {
+	own := list.New()
+	for i := 0; i < cnt; i++ {
+		own.PushBack(elf{index: i, value: 1})
 	}
+	curr := own.Front()
+	for {
+		old := curr
+		next := WrapNext(curr, own)
+		updated := elf{value: curr.Value.(elf).value + next.Value.(elf).value, index: curr.Value.(elf).index}
+		own.Remove(old)
+		curr = own.InsertBefore(updated, next)
+		own.Remove(next)
+		if curr.Value.(elf).value == cnt {
+			return fmt.Sprint(curr.Value.(elf).index + 1)
+		}
+		curr = WrapNext(curr, own)
+	}
+}
+
+func part2(cnt int) string {
+	own := list.New()
+	for i := 0; i < cnt; i++ {
+		own.PushBack(elf{index: i, value: 1})
+	}
+	curr := own.Front()
+	next := curr
+	for i := 0; i < own.Len()/2; i++ {
+		next = WrapNext(next, own)
+	}
+	for i := 0; ; i++ {
+		old := curr
+		updated := elf{value: curr.Value.(elf).value + next.Value.(elf).value, index: curr.Value.(elf).index}
+		// fmt.Printf("%+v stealing from %v\n", updated, next.Value)
+		insertBefore := WrapNext(curr, own)
+		curr = own.InsertBefore(updated, insertBefore)
+		nnext := WrapNext(next, own)
+		own.Remove(old)
+		own.Remove(next)
+		if own.Len()%2 == 0 {
+			nnext = WrapNext(nnext, own)
+		}
+		if curr.Value.(elf).value == cnt {
+			return fmt.Sprint(curr.Value.(elf).index + 1)
+		}
+		curr = WrapNext(curr, own)
+		next = nnext
+	}
+}
+
+func WrapNext(e *list.Element, l *list.List) *list.Element {
+	if e.Next() == nil {
+		return l.Front()
+	}
+	return e.Next()
 }
