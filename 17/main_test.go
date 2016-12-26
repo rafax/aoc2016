9adfbc1d9da6d18414bd77d48bb21c0d9af9da52
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-12-26 19:26:08 +0100

    Fix test

--- 17/main_test.go
+++ 17/main_test.go
@@ -2,21 +2,21 @@ package main
 
 import "testing"
 
-var neg = "0123456789a"
-var pos = "bcdef"
+var negative = "0123456789a"
+var positive = "bcdef"
 
 func TestIsOpenNegative(t *testing.T) {
-	for i := range neg {
-		if isOpen(neg[i]) {
-			t.Errorf("Expected %v got %v for %v", false, true, string(neg[i]))
+	for i := range negative {
+		if isOpen(negative[i]) {
+			t.Errorf("Expected %v got %v for %v", false, true, string(negative[i]))
 		}
 	}
 }
 
 func TestIsOpenPositive(t *testing.T) {
-	for i := range pos {
-		if !isOpen(pos[i]) {
-			t.Errorf("Expected %v got %v for %v", false, true, string(neg[i]))
+	for i := range positive {
+		if !isOpen(positive[i]) {
+			t.Errorf("Expected %v got %v for %v", true, false, string(positive[i]))
 		}
 	}
 }
